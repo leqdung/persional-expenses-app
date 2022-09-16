97cab555b563f1324e7ea30eb2786e04a530cd5e
Apple <apple@MacBook-Air.local>
2022-09-17 04:52:35 +0700

    pubspec.yaml edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -6,18 +7,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   final List<Transaction> transaction = [
     Transaction(
-      title: 'shose',
+      title: 'Shose',
       id: 't1',
-      amount: 69.0,
+      amount: 69.99,
       date: DateTime.now(),
     ),
     Transaction(
-      title: 'bag',
+      title: 'Bag',
       id: 't2',
-      amount: 45.0,
+      amount: 45.92,
       date: DateTime.now(),
     )
   ];
+
+  // String? titleInput;
+  // String? amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,25 +33,97 @@ class MyApp extends StatelessWidget {
           title: Text('My App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
               width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('Hello world'),
-                elevation: 5,
+              child: Card(),
+            ),
+            Card(
+              elevation: 4,
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                margin: EdgeInsets.all(5.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Title:',
+                      ),
+                      controller: titleController,
+                      // onChanged: (val) => titleInput = val,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Amount:',
+                      ),
+                      controller: amountController,
+                      // onChanged: (val) => amountInput = val,
+                    ),
+                    ElevatedButton(
+                      child: Text('Add transactions'),
+                      // textColor: Colors.purple,
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.red,
-                child: Text('This is List'),
-                elevation: 5,
-              ),
-            ),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  // child: Text(tx.title),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: 15.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2.0,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('EEE, yy-mm-dd').format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
